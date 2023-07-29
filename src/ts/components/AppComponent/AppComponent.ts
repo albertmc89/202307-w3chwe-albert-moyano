a97b4f8763a1c54448e61cdd88c4c6b8469ea968
Albert Colubi <albertmc89@gmail.com>
2023-07-30 01:55:15 +0200

    Render pokemon cards to AppComponent

--- src/ts/components/AppComponent/AppComponent.ts
+++ src/ts/components/AppComponent/AppComponent.ts
@@ -1,8 +1,19 @@
+import pokemonListData from "../../pokemonListData.js";
+import { type PokemonProfile } from "../../types.js";
 import Component from "../Component/Component.js";
+import PokemonCardComponent from "../PokemonCardComponent/PokemonCardComponent.js";
 
 class AppComponent extends Component {
+  private singlePokemon: PokemonProfile[] = [];
+
   constructor(parentElement: Element) {
     super(parentElement, "div", "container");
+    (async () => {
+      const pokemones: PokemonProfile[] = await pokemonListData();
+
+      this.singlePokemon = pokemones;
+      this.renderPokemonCards();
+    })();
   }
 
   public render(): void {
@@ -16,6 +27,15 @@ class AppComponent extends Component {
         </ul>
       </main>
     `;
+  }
+
+  private renderPokemonCards() {
+    const pokemonListElement = this.element.querySelector(".pokemon-list")!;
+    this.singlePokemon.forEach((singlePokemon) => {
+      const pokemonCardListElement = document.createElement("li");
+      new PokemonCardComponent(pokemonCardListElement, singlePokemon).render();
+      pokemonListElement.append(pokemonCardListElement);
+    });
   }
 }
 
